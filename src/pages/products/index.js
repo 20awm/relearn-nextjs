0c088d6233913be58c035917ebf7b6d28df4694d
alifwiseman <alifwiseman@gmail.com>
2024-10-02 13:40:16 +0700

    update useRef

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,6 +1,6 @@
 import Button from "@/components/atoms/Button";
 import CardProduct from "@/components/molecules/CardProduct";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const data = [
   {
@@ -33,12 +33,8 @@ function ProductsPage() {
   const [username, setUsername] = useState("");
   const [cart, setCart] = useState([]);
   const [total, setTotal] = useState(0);
-  /** useState : hooks dari react yang memungkinkan kita menambahkan state ke functional component.
-   * username : variabel state yang akan menyimpan nilai username
-   * setUsername : fungsi yang dipake buat memperbarui nilai username
-   * "" (string kosong dalam useState("")) : nilai awal dari state username
-   * ketika setUsername dipanggil dengan nilai baru, react akan me-render ulang komponen dengan nilai state yang baru
-   */
+  const [searchTerm, setSearchTerm] = useState("");
+  const searchInputRef = useRef(null);
 
   useEffect(() => {
     const getUsername = localStorage.getItem("username");
@@ -46,13 +42,8 @@ function ProductsPage() {
       setUsername(getUsername);
     }
     setCart(JSON.parse(localStorage.getItem("cart")) || []);
+    searchInputRef.current.focus();
   }, []);
-  /** useEffect(()=>{}, []) : hooks dari react yang memungkinkan kita untuk menambahkan side effect ke functional component
-   * useEffect dipake untuk memperbarui komponen ketika ada perubahan pada state.
-   * [] (array kosong/dependency array) : argumen kedua milik useEffect yang artinya efek ini akan dijalankan sekali
-   saat komponen pertama kali dirender(load).
-   jika username ditemukan di localStorage, ia menggunakan setUsername untuk memperbarui nilai dari state username
-   */
 
   const handleLogout = () => {
     localStorage.removeItem("username");
@@ -61,16 +52,13 @@ function ProductsPage() {
   };
 
   const handleAddToCart = (id) => {
-    // jika ada id yang sama maka akan menambahkan jumlah qty +1
     if (cart.find((item) => item.id === id)) {
-      //dia akan mapping dan membongkar itemnya untuk mencari data dengan id yg sama
       setCart(
         cart.map((item) =>
           item.id === id ? { ...item, qty: item.qty + 1 } : item
         )
       );
     } else {
-      // kalo data nya cma 1 maka cma akan di set satu
       setCart([...cart, { id, qty: 1 }]);
     }
   };
@@ -86,6 +74,10 @@ function ProductsPage() {
     }
   }, [cart]);
 
+  const filteredProducts = data.filter((product) =>
+    product.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <>
       <div className="flex justify-between items-center bg-blue-500 px-5 py-4">
@@ -93,11 +85,18 @@ function ProductsPage() {
         <Button color="bg-red-500" textButton="Logout" onClick={handleLogout} />
       </div>
       <div className="flex px-5 py-4">
-        <div className="flex flex-col">
+        <div className="flex flex-col w-2/3">
           <h1 className="text-3xl font-bold mb-2 uppercase">Products</h1>
-          {/* products */}
+          <input
+            ref={searchInputRef}
+            type="text"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder="Search for a product"
+            className="mb-4 p-2 border border-gray-300"
+          />
           <div className="flex flex-wrap gap-4">
-            {data.map((item) => (
+            {filteredProducts.map((item) => (
               <CardProduct key={item.id}>
                 <CardProduct.Header image={item.image} />
                 <CardProduct.Body title={item.name} desc={item.desc} />
@@ -115,42 +114,37 @@ function ProductsPage() {
             <h1 className="text-3xl font-bold mb-2 uppercase">Cart</h1>
             <div className="flex flex-col gap-2">
               {cart.map((item) => {
-                // logic untuk nyari id dalam variabel data, kalo id yg di data sama dengan id yg ada di cart maka ambil produknya
                 const datas = data.find((data) => data.id === item.id);
                 return (
-                  <>
-                    <div key={item.id} className="flex p-4 border rounded-lg">
-                      <img
-                        src={datas.image}
-                        alt="cart item"
-                        className="max-w-[100px]"
-                      />
-                      <div className="flex justify-between w-full">
-                        <div className="flex flex-col justify-between ml-3">
-                          <span className="font-bold text-xl">
-                            {datas.name}
-                          </span>
-                          <span className="font-semibold">
-                            {(datas.price * item.qty).toLocaleString("id-ID", {
-                              style: "currency",
-                              currency: "IDR",
-                            })}
-                          </span>
-                        </div>
-                        <div className="flex flex-col justify-center items-center">
-                          <span className="mb-1">Qty</span>
-                          <span className="flex-justify-center items-center font-semibold p-2 border rounded-sm text-center w-10 h-10">
-                            {item.qty}
-                          </span>
-                        </div>
+                  <div key={item.id} className="flex p-4 border rounded-lg">
+                    <img
+                      src={datas.image}
+                      alt="cart item"
+                      className="max-w-[100px]"
+                    />
+                    <div className="flex justify-between w-full">
+                      <div className="flex flex-col justify-between ml-3">
+                        <span className="font-bold text-xl">{datas.name}</span>
+                        <span className="font-semibold">
+                          {(datas.price * item.qty).toLocaleString("id-ID", {
+                            style: "currency",
+                            currency: "IDR",
+                          })}
+                        </span>
+                      </div>
+                      <div className="flex flex-col justify-center items-center">
+                        <span className="mb-1">Qty</span>
+                        <span className="flex-justify-center items-center font-semibold p-2 border rounded-sm text-center w-10 h-10">
+                          {item.qty}
+                        </span>
                       </div>
                     </div>
-                  </>
+                  </div>
                 );
               })}
             </div>
             <div className="flex justify-between px-4 py-2 border mt-2">
-              <span className="font-semibold">total</span>
+              <span className="font-semibold">Total</span>
               <span className="font-semibold">
                 {total.toLocaleString("id-ID", {
                   style: "currency",
