70941f30f7ba58ea0830a2ba8d1d74af2bbfa808
alifwiseman <alifwiseman@gmail.com>
2024-10-04 16:18:44 +0700

    static site generation

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -179,21 +179,7 @@ function ProductsPage({ products }) {
   );
 }
 
-export async function getServerSideProps() {
-  // Method 1
-  // try {
-  //   const products = await getProducts();
-  //   const slicedProducts = products.slice(0, 8);
-  //   return {
-  //     props: {
-  //       products: slicedProducts || [],
-  //     },
-  //   };
-  // } catch (error) {
-  //   console.log(error);
-  // }
-
-  // Method 2
+export async function getStaticProps() {
   try {
     const [productResults] = await Promise.all([getProducts()]);
     const slicedProducts = productResults.slice(0, 8);
